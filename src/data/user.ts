e216a6666b512e69900a22cc8305828b6e634eeb
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-12-26 02:55:59 +0530

    add database calls for the user

---
+++ src/data/user.ts
@@ -0,0 +1,43 @@
+import db from "@/lib/db";
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    return user;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return user;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export const createNewUser = async (
+  name: string,
+  email: string,
+  hashedPassword: string,
+) => {
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+};
