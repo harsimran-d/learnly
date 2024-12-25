832be9ed0dff79e79ba222556ce89b1ef1aad258
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-12-26 02:56:31 +0530

    update the register action to create accounts

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,12 +2,29 @@
 import * as z from "zod";
 
 import { RegisterSchema } from "@/schemas";
-import { setTimeout } from "timers/promises";
+import bcrypt from "bcrypt";
+
+import { createNewUser, getUserByEmail } from "@/data/user";
+
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  await setTimeout(3000);
-  const validatedFields = RegisterSchema.safeParse(values);
-  if (!validatedFields.success) {
-    return { error: "Invalid fields!" };
+  try {
+    const validatedFields = RegisterSchema.safeParse(values);
+    if (!validatedFields.success) {
+      return { error: "Invalid fields!" };
+    }
+    const { name, email, password } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      return { error: "Email already in use" };
+    }
+    await createNewUser(name, email, hashedPassword);
+    // TODO: Send verification email token
+    return { success: "Account Created Successfully" };
+  } catch (e) {
+    console.log(e);
+    return { error: "Something went wrong" };
   }
-  return { success: "Email sent!" };
 };
