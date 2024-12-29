f7015592d7a486616f8f2fb8c80332ed29d503a6
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-12-29 06:47:32 +0530

    add shadcn toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 import type { Metadata } from "next";
 
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <Toaster />
     </html>
   );
 }
