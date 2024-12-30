b70d0dad78ef533a725bfc1b6349cb0a13c222e9
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-12-30 14:22:15 +0530

    chore: dead code clean up
    remove the is teacher console log and variable in the navbar component

--- src/app/(protected)/_components/navbar.tsx
+++ src/app/(protected)/_components/navbar.tsx
@@ -1,15 +1,7 @@
 import NavbarRoutes from "@/components/navbar-routes";
 import { MobileSidebar } from "./mobile-sidebar";
-import { auth } from "@/lib/auth";
-import { CustomSession } from "@/auth.config";
 
 const Navbar = async () => {
-  const session = (await auth()) as CustomSession;
-  let isTeacher = false;
-  if (session?.user?.role == "TEACHER") {
-    isTeacher = true;
-  }
-  console.log(isTeacher);
   return (
     <div className="flex h-full items-center border-b bg-white p-2 shadow-sm">
       <MobileSidebar />
