cfc97adee23cd6d2f247a4e1d8b9b773ddab0602
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-12-31 18:20:43 +0530

    add: route for course id patch

---
+++ src/app/api/courses/[courseId]/route.ts
@@ -0,0 +1,30 @@
+import { auth } from "@/lib/auth";
+import db from "@/lib/db";
+import { NextResponse } from "next/server";
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string } },
+) {
+  try {
+    const userId = (await auth())?.user?.id;
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const { courseId } = params;
+    const values = await req.json();
+
+    await db.course.update({
+      where: {
+        id: courseId,
+        teacherId: userId,
+      },
+      data: {
+        ...values,
+      },
+    });
+    return new NextResponse("Course updated successfully", { status: 200 });
+  } catch (e) {
+    console.error("[COURSE_ID PATCH] ", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -1,7 +1,7 @@
 import { CustomUser } from "@/auth.config";
 import { auth } from "@/lib/auth";
-import { NextResponse } from "next/server";
 import db from "@/lib/db";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
