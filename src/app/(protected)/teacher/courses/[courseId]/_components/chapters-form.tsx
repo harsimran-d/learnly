33796b303525c9264587ff073b05d15d8dc3f76f
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2025-01-03 22:30:08 +0530

    add: status chip on the chapters list form

--- src/app/(protected)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ src/app/(protected)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { $Enums } from "@prisma/client";
 import Link from "next/link";
 
@@ -13,6 +14,20 @@ interface ChaptersFormProps {
     }[];
   };
 }
+
+const getStatusClasses = (status: $Enums.PublishStatus) => {
+  switch (status) {
+    case "DRAFT":
+      return "bg-blue-200 text-blue-700";
+    case "PUBLISHED":
+      return "bg-green-200 text-green-700";
+    case "ARCHIVED":
+      return "bg-gray-200 text-gray-700";
+    default:
+      return "bg-white text-black";
+  }
+};
+
 const ChaptersForm = ({ courseId, initialData }: ChaptersFormProps) => {
   return (
     <div className="rounded-md border bg-slate-100 p-6">
@@ -25,11 +40,21 @@ const ChaptersForm = ({ courseId, initialData }: ChaptersFormProps) => {
           return (
             <div key={chapter.id} className="flex items-center justify-between">
               <p>{chapter.title}</p>
-              <Link
-                href={`/teacher/courses/${courseId}/chapters/${chapter.id}`}
-              >
-                <Button variant="ghost">Edit</Button>
-              </Link>
+              <div className="flex items-center">
+                <div
+                  className={cn(
+                    "rounded-xl px-2 py-1 text-xs",
+                    getStatusClasses(chapter.status),
+                  )}
+                >
+                  {chapter.status}
+                </div>
+                <Link
+                  href={`/teacher/courses/${courseId}/chapters/${chapter.id}`}
+                >
+                  <Button variant="ghost">Edit</Button>
+                </Link>
+              </div>
             </div>
           );
         })
